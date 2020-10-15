57eb3da35f8701e8c558acc12afb16e19222bd6b
Ayush Mehre <ayushmehre@gmail.com>
2020-10-16 01:12:01 +0530

    made changes

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -262,7 +262,7 @@ class _WelcomeState extends State<Welcome> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text(
-              "V.1.0",
+              "V.1.1",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
           ),
