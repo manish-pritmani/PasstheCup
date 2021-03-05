22cc8d30195c5297a9b6786c6eed128ae93ed8c1
ayushglobalit24 <Ayush@globalit24.com>
2021-03-05 20:42:57 +0530

    made changes

--- lib/api.dart
+++ lib/api.dart
@@ -307,8 +307,8 @@ class API {
     List<GameObject> list;
     DateTime EastCoast = DateTime.now().toUtc().add(Duration(hours: -4));
 //    curDateTimeByZone(zone: "EST");
-//     var date = DateFormat("yyyy-MMM-dd").format(EastCoast);// "2020-SEP-27";
-    var date = "2020-SEP-27";
+    var date = DateFormat("yyyy-MMM-dd").format(EastCoast); // "2020-SEP-27";
+    // var date = "2020-SEP-27";
     var url =
         "https://api.sportsdata.io/v3/mlb/scores/json/GamesByDate/$date?key=5863b9d2fa7746cd8495fb3cc4b53743"; //BaseUrl + "Games/2020REG" + keyString;
     final response =
@@ -393,7 +393,7 @@ class API {
   }
 
   Future<FullPlayerObject> fetchPlayerProfile(int hitterID) async {
-    if(hitterID==null){
+    if (hitterID == null) {
       hitterID = 10000001;
     }
     var url =
@@ -434,26 +434,26 @@ class API {
     }
   }
 
-
   Future<bool> startSimulation(int gameID, String gameCode) async {
     var checkURL = checkStatus + gameID.toString();
 
-    var stopURL =
-        'http://161.8.42.170:8181/PTCGame/UpadteGameStatusInDB?GameId=$gameID&Status=-1';
-    final response = await http.get(stopURL);
-    print(response.body);
+    // var stopURL =
+    //     'http://161.8.42.170:8181/PTCGame/UpadteGameStatusInDB?GameId=$gameID&Status=-1';
+    // final response = await http.get(stopURL);
+    // print(response.body);
 
     var response2 = await http.get(checkURL);
-    if (response2.body == "0") {
+    if (response2.body != "-1") {
+      print('simulation already running');
       return false;
+    } else {
+      var simURL =
+          'http://161.8.42.170:8181/PTCGame/UpadteGameStatusInDB?GameId=$gameID&Status=1';
+      final response1 = await http.get(simURL);
+      print(response1.body);
+
+      return true;
     }
-
-    var simURL =
-        'http://161.8.42.170:8181/PTCGame/UpadteGameStatusInDB?GameId=$gameID&Status=1';
-    final response1 = await http.get(simURL);
-    print(response1.body);
-
-   return true;
   }
 
   Future<List<TeamObject>> fetchTeamImage() async {
